047de4c1245846fc8e3f93f61d1006bfbcea2e10
KhenCahyo13 <khencahyo02@gmail.com>
2023-05-01 00:25:47 +0700

    fix bug host

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { routes } from './routes.js'
 const init = async () => {
   const server = Server({
     port: 5000,
-    host: 'localhost',
+    host: '172.31.28.131',
     routes: {
         cors: {
             origin: ['*']
